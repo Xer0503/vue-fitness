cced4798fe14953d3a2b676a3fa920f874f845f8
Rexie N. Villanueva <rexievillanueva03@gmail.com>
2025-07-21 09:43:15 +0800

    Add bars-solid.svg icon from Font Awesome Free 6.7.2

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -5,7 +5,7 @@
         <div class="px-3">
 
             <form action="#">
-                <div class="flex flex-col gap-5">
+                <div id="contact" class="flex flex-col gap-5">
                     <h1 class="font-bold text-3xl">Contact Us</h1>
                     <input class="border px-2 py-2 w-auto shadow rounded-2xl" type="text" placeholder="email">
                     <textarea class="border shadow px-2 py-3 rounded" name="" id="" cols="3" placeholder="message here"></textarea>
@@ -16,11 +16,11 @@
             </form>
         </div>
 
-        <div>
+        <div id="about">
             <ul>
                 <li>
                     <i class="fa-brands fa-github"></i>
-                    <a href="#" >
+                    <a href="#about" >
                         Xer0503
                     </a>
                 </li>
--- src/components/layout/Nav.vue
+++ src/components/layout/Nav.vue
@@ -1,23 +1,52 @@
 <script setup>
+import { ref } from 'vue'
+import Bar from '../../assets/Bar.png'
+
+const isOpen = ref(false)
 </script>
 
 <template>
-    <div class="nav flex justify-between items-center px-5 py-2">
-        <span class="text-2xl font-bold">
-            GYM<span class="text-orange-600">SICK</span>
-        </span>
-        <div>
-            <ul class="text-center flex space-x-1 md:space-x-6 text-sm">
-                <li class="hover:underline hover:text-orange-600"><a href="#">Home</a></li>
-                <li class="hover:underline hover:text-orange-600"><a href="#">About Us</a></li>
-                <li class="hover:underline hover:text-orange-600"><a href="#">Pricing</a></li>
-                <li class="hover:underline hover:text-orange-600"><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        <span>
-            <button class="px-2 py-1 bg-orange-600 text-white rounded text-sm">Start Today</button>
-        </span>
+  <div class="nav flex justify-between items-center px-5 py-2">
+    <!-- Logo -->
+    <span class="text-2xl font-bold">
+      GYM<span class="text-orange-600">SICK</span>
+    </span>
+
+    <!-- Desktop Nav -->
+    <div class="hidden md:block">
+      <ul class="text-center flex space-x-1 md:space-x-6 text-md">
+        <li class="hover:underline hover:text-orange-600 hover:font-semibold"><a href="#">Home</a></li>
+        <li class="hover:underline hover:text-orange-600 hover:font-semibold"><a href="#about">About Us</a></li>
+        <li class="hover:underline hover:text-orange-600 hover:font-semibold"><a href="#">Pricing</a></li>
+        <li class="hover:underline hover:text-orange-600 hover:font-semibold"><a href="#contact">Contact</a></li>
+      </ul>
     </div>
+
+    <!-- Desktop Button -->
+    <span class="hidden md:block">
+      <button class="px-2 py-1 w-35 bg-orange-600 text-white rounded text-sm hover:opacity-85">Start Today</button>
+    </span>
+
+    <!-- Mobile Hamburger Icon -->
+    <div class="block md:hidden">
+      <button @click="isOpen = !isOpen">
+        <img :src="Bar" alt="Nav Logo" loading="lazy" class="mx-auto w-10" />
+      </button>
+    </div>
+  </div>
+
+  <!-- Mobile Menu -->
+  <div v-if="isOpen" class="md:hidden px-5 pb-4 text-center">
+    <ul class="flex flex-col space-y-2 text-md">
+      <li class="hover:underline hover:text-orange-600 hover:font-semibold active:text-orange-600"><a href="#">Home</a></li>
+      <li class="hover:underline hover:text-orange-600 hover:font-semibold active:text-orange-600"><a href="#about">About Us</a></li>
+      <li class="hover:underline hover:text-orange-600 hover:font-semibold active:text-orange-600"><a href="#">Pricing</a></li>
+      <li class="hover:underline hover:text-orange-600 hover:font-semibold active:text-orange-600"><a href="#contact">Contact</a></li>
+      <li>
+        <button class="w-35 mt-2 px-2 py-1 bg-orange-600 text-white rounded text-sm">Start Today</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
